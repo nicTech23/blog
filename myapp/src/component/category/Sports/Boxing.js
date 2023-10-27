1b19d777f88c4baa92a8b5a717fd52dfa7828d1b
Nicholas <nictech23@gmail.com>
2023-10-27 00:23:55 +0000

    link

--- myapp/src/component/category/Sports/Boxing.js
+++ myapp/src/component/category/Sports/Boxing.js
@@ -2,15 +2,15 @@ import { Box, CardMedia } from '@mui/material'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchSport } from '../../redux/createSlice/allStoriesSlice'
+import { Link } from 'react-router-dom'
 
 const Boxing = () => {
   const data = useSelector((state)=> state.allPostData.sports.data.data)
   const status = useSelector((state)=> state.allPostData.sports.status)
   
   const dispatch = useDispatch()
-  console.log(data)
-  console.log(status)
-  //Function to get only boxing stories
+  
+  //Function to get only boxing stories and it return array
   const boxingObjects = ()=>{
     if(data){
         return data.filter(item => item.category.some(category => category.specific.includes("boxing")&&category.name === 'sports'));
@@ -31,17 +31,20 @@ const boxingData = boxingObjects();
     <Box className='w-full grid lg:grid-cols-4 gap-4 sm:grid-cols-2'>
           {
             boxingData?boxingData.map(data =>{
+              let url = `/${data.category[0].name}/${data.title}`
               return(
-                <Box className='bg-gray-200 h-64 overflow-hidden hover:bg-red-600 hover:text-white trasnsition ease-in-out'>
-                  <CardMedia className=''sx={{height:170, objectFit:'cover'}}
-                              component="img"
-                              image="https://www.graphic.com.gh/images/2022/jan/04/akufo_addo1.jpg"
-                              alt="green iguana"
-                              />
-                  <Box  className='m-3 overflow-hidden'>
-                    <span className='text-sm'>{data.content}</span>
+                <Link key={data._id} to={url}>
+                  <Box className='bg-gray-200 h-64 overflow-hidden hover:bg-red-600 hover:text-white trasnsition ease-in-out'>
+                    <CardMedia className=''sx={{height:170, objectFit:'cover'}}
+                                component="img"
+                                image="https://www.graphic.com.gh/images/2022/jan/04/akufo_addo1.jpg"
+                                alt="green iguana"
+                                />
+                    <Box  className='m-3 overflow-hidden'>
+                      <span className='text-sm'>{data.content}</span>
+                    </Box>
                   </Box>
-                </Box>
+                </Link>
               )
             }):null
           }
--- myapp/src/component/singlePage/SinglePage.js
+++ myapp/src/component/singlePage/SinglePage.js
@@ -1,5 +1,5 @@
 import { Avatar, Box, Button, CardMedia, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Cardbox from './Cardbox'
 import SidebarRight from './../sidebarRight/SidebarRight';
 import { useParams } from 'react-router-dom';
@@ -16,39 +16,34 @@ const SinglePage = () => {
   const status = useSelector((state)=> state.singleData.status)
   const dispatch = useDispatch()
    
-
-  const GetData = createAsyncThunk('singlePost/fetch', async()=>{
-    const response = axios.get(`http://localhost:8080/api/v1/${category}/${title}`)
-    return response
-  })
-
   const {category, title} = useParams()
   
-  if(status === 'idle'){
-    axios.get(`http://localhost:8080/api/v1/singledata/${category}/${title}`)
-    .then((response)=>{
-      dispatch(singleDataAction.singleData(response))
-      console.log(data, status)
-    })
+  const GetData = async()=>{
+    try {
+        const response = await axios.get(`http://localhost:8080/api/v1/singledata/${category}/${title}`)
+        const responseData = await response.data
+        dispatch(singleDataAction.singleData(responseData))
+    } catch (error) {
+      return error.message
+    }
   }
 
+  useEffect(()=>{
+      GetData()
+  }, [dispatch, status])
+
+  console.log(data)
   return (
     <Box className='w-full flex flex-row justify-center pl-40 pr-40 sm:pl-0 sm:pr-0'sx={{paddingLeft:{xs:'2px', lg:'10rem'}, paddingRight:{xs:'2px', lg:'10rem'}}} > 
+    {/* {data ? <div dangerouslySetInnerHTML={{__html: data.data.content}}/>: null} */}
         <Box className='w-2/3 pr-10 sm:pr-5 sm:w-full' sx={{paddingLeft:{xs:'2px'}, paddingRight:{xs:'2px', lg:"2.5rem"}, width:{xs:'90%', lg:"100%"}}}>
+         {data && data.data && data.data.title ? <div className='text-3xl capitalize font-black, font-sans' dangerouslySetInnerHTML={{__html: data.data.title.split('-').join(' ')}}/>: null} 
           <CardMedia className='rounded-2xl w-full overflow-hidden mt-5 mb-5' sx={{objectFit:'cover', height:{xs:'400px', lg:'500px'}}}
                         component="img"
                         image="https://www.graphic.com.gh/images/2022/jan/04/akufo_addo1.jpg"
                         alt="green iguana"
                         />
-            <Typography className='text text-justify'>
-                However the specification actually allows other event-triggered code to continue to execute even though a modal
-                dialog is still being shown. In such implementations, it is possible for other code to run while the modal dialog is
-                being shown.
-                More information about usage of the alert method can be found in the modals prompts topic.
-                The use of alerts is usually discouraged in favour of other methods that do not block users from interacting with the
-                page - in order to create a better user experience. Nevertheless, it can be useful for debugging.
-                Starting with Chrome 46.0, window.alert() is blocked inside an 
-            </Typography>
+             {data ? <Typography className='text text-justify' dangerouslySetInnerHTML={{__html: data.data.content}}/>: null}
             <Box className='mt-10 flex flex-col'>
               <Typography sx={{fontSize:"18px", fontWeight:'bold'}}>Let us have your comment</Typography>
               <textarea rows={4} cols={5} className='w w-3/4 border border-1 hover:border-2 outline-0 pl-5 border-red-500 rounded-2xl'/>
